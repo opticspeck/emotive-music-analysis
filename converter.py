4f425208c9e01302dc039bce48de687d65fb5aa4
Mel Mark <opticspeck@gmail.com>
2019-11-15 23:43:39 -0600

    fixed some issues and added a new tool
    the current issue is getting weka to accept the trash number put in by the non-numerical digits in the metadata section of the Essentia JSON. I need to go to bed lol but I think the next step would be running the string through a giant for loop w/nested conditionals seeing if the space between two commas is greater than a certain number (aka the trash attribute). also, I added a new tool for helping me generate the attribute list in the base.arff file

--- converter.py
+++ converter.py
@@ -1,8 +1,10 @@
-# Written by Mel Mark, Park University 2019, to convert JSON files to ARFF files for the ML app, Weka.
+# Written by Mel Mark, Park University 2019
+# A tool to convert JSON files to ARFF files for the machine learning app, Weka.
 # https://m3l.me
 
 import json
 
+header = ""
 just_nums = ""
 end_str = ""
 list_of_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -13,7 +15,17 @@ with open('file_to_read.json') as json_file:
     json_values_as_list = json_data.values()
     values = str(json_values_as_list)
     for x in range(len(values)):
-        if values[x] in list_of_nums and values[x + 1] == ',':
+        # this accounts for an issue I found when collecting the metadata information since digits can exist in
+        # non-numerical variables like strings. this is not a very elegant way of going about it but it should work
+        # since bpms should be between 1-3 digits.
+        if values[x] == 'u' and values[x + 1] == 'n' and values[x + 2] == 't':
+            if (values[x + 5] == ' ' and values[x + 6] in list_of_nums and values[x + 7] == ',') or\
+                    (values[x + 5] == ' ' and values[x + 6] in list_of_nums and values[x + 7] in list_of_nums and
+                     values[x + 8] == ',') or (values[x + 5] == ' ' and values[x + 6] in list_of_nums and
+                                               values[x + 7] in list_of_nums and values[x + 8] in list_of_nums and
+                                               values[x + 9] == ','):
+                just_nums += ', '
+        elif values[x] in list_of_nums and (values[x + 1] == ',' or values[x + 1] == '}' or values[x + 1] == ']'):
             just_nums += values[x] + ', '
         elif values[x] in list_of_mums_and_dot:
             just_nums += values[x]
