3d7bfb9cab00ab3f312ee49c40dfcbc8a4a17b0e
Mel Mark <opticspeck@gmail.com>
2020-03-26 18:57:12 -0500

    black formatting
    functionally nothing changing, just formatting under [black](https://black.readthedocs.io/en/stable/)

--- arff_attribute_enumerator.py
+++ arff_attribute_enumerator.py
@@ -2,69 +2,153 @@
 # A tool to help me out generating lots of lines of attributes for the converter outline.
 # https://m3l.me
 
-repeat = 'y'
+repeat = "n"
 name_of_attribute = ""
 # attributes that have the most common set of variables (see following list)
-normal_attributes = ['barkbands', 'erbbands', 'melbands', 'melbands128', 'spectral_contrast_coeffs',
-                     'spectral_contrast_valleys', 'beats_loudness_band_ratio', 'hpcp', 'hpcp_crest']
-normal_variables = ['dmean', 'dmean2', 'dvar', 'dvar2', 'max', 'mean', 'median', 'min', 'stdev', 'var']
+normal_attributes = [
+    "barkbands",
+    "erbbands",
+    "melbands",
+    "melbands128",
+    "spectral_contrast_coeffs",
+    "spectral_contrast_valleys",
+    "beats_loudness_band_ratio",
+    "hpcp",
+    "hpcp_crest",
+]
+normal_variables = [
+    "dmean",
+    "dmean2",
+    "dvar",
+    "dvar2",
+    "max",
+    "mean",
+    "median",
+    "min",
+    "stdev",
+    "var",
+]
 # attributes that only have itself :(
-lonely_attributes = ['beats_position', 'bpm_histogram', 'chords_histogram', 'thpcp']
+lonely_attributes = ["beats_position", "bpm_histogram", "chords_histogram", "thpcp"]
 # attributes that have the same set of three variables (see following list)
-cc_attributes = ['gfcc', 'mfcc']
-cc_variables = ['mean', 'cov', 'icov']
+cc_attributes = ["gfcc", "mfcc"]
+cc_variables = ["mean", "cov", "icov"]
 
 
 def generate_normal_attributes(variable_name):
     if number_of_commas > 0:
         for x in range(number_of_commas + 1):
-            print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.' + variable_name + '_' + str(
-                x) + '    REAL')
+            print(
+                "@attribute     "
+                + type_of_attribute
+                + "."
+                + name_of_attribute
+                + "."
+                + variable_name
+                + "_"
+                + str(x)
+                + "    REAL"
+            )
     else:
-        print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.' + variable_name + '    REAL')
+        print(
+            "@attribute     "
+            + type_of_attribute
+            + "."
+            + name_of_attribute
+            + "."
+            + variable_name
+            + "    REAL"
+        )
 
 
-def generate_cc_attributes(number_of_mean_commas, number_of_cov_commas, number_of_icov_commas):
+def generate_cc_attributes(
+    number_of_mean_commas, number_of_cov_commas, number_of_icov_commas
+):
     for x in range(number_of_mean_commas):
-        print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.mean_' + str(x) + '    REAL')
+        print(
+            "@attribute     "
+            + type_of_attribute
+            + "."
+            + name_of_attribute
+            + ".mean_"
+            + str(x)
+            + "    REAL"
+        )
     for x in range(number_of_cov_commas):
-        print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.cov_' + str(x) + '    REAL')
+        print(
+            "@attribute     "
+            + type_of_attribute
+            + "."
+            + name_of_attribute
+            + ".cov_"
+            + str(x)
+            + "    REAL"
+        )
     for x in range(number_of_icov_commas):
-        print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.icov_' + str(x) + '    REAL')
+        print(
+            "@attribute     "
+            + type_of_attribute
+            + "."
+            + name_of_attribute
+            + ".icov_"
+            + str(x)
+            + "    REAL"
+        )
 
 
-while repeat == 'y':
-    type_of_attribute = input('what type of attribute is it? (ll/r/t)\t')
-    name_of_attribute = input('name of attribute?\t')
+beats_pos = input("how many beats positions\t")
+count = 118 - int(beats_pos)
+bleh = ""
+for x in range(count):
+    if x != count - 1:
+        bleh += "0, "
+    else:
+        bleh += "0"
+print(bleh)
+
+while repeat == "y":
+    type_of_attribute = input("what type of attribute is it? (ll/r/t)\t")
+    name_of_attribute = input("name of attribute?\t")
     name_of_attribute = name_of_attribute.lower()
 
-    if type_of_attribute == 'll':
-        type_of_attribute = 'lowlevel'
-    elif type_of_attribute == 'r':
-        type_of_attribute = 'rhythm'
-    elif type_of_attribute == 't':
-        type_of_attribute = 'tonal'
+    if type_of_attribute == "ll":
+        type_of_attribute = "lowlevel"
+    elif type_of_attribute == "r":
+        type_of_attribute = "rhythm"
+    elif type_of_attribute == "t":
+        type_of_attribute = "tonal"
     else:
-        print('invalid type of attribute >:(')
+        print("invalid type of attribute >:(")
         break
 
     if name_of_attribute in normal_attributes:
-        number_of_commas = int(input('how many commas are there?\t'))
+        number_of_commas = int(input("how many commas are there?\t"))
         for x in range(len(normal_variables)):
             generate_normal_attributes(normal_variables[x])
     elif name_of_attribute in lonely_attributes:
-        number_of_commas = int(input('how many commas are there?\t'))
+        number_of_commas = int(input("how many commas are there?\t"))
         for x in range(number_of_commas):
-            print('@attribute     ' + type_of_attribute + '.' + name_of_attribute + '.' + '_' + str(x) + '    REAL')
+            print(
+                "@attribute     "
+                + type_of_attribute
+                + "."
+                + name_of_attribute
+                + "."
+                + "_"
+                + str(x)
+                + "    REAL"
+            )
     elif name_of_attribute in cc_attributes:
-        number_of_mean_commas = int(input('how many MEAN commas are there?\t'))
-        number_of_cov_commas = int(input('how many COV commas are there?\t'))
-        number_of_icov_commas = int(input('how many ICOV commas are there?\t'))
+        number_of_mean_commas = int(input("how many MEAN commas are there?\t"))
+        number_of_cov_commas = int(input("how many COV commas are there?\t"))
+        number_of_icov_commas = int(input("how many ICOV commas are there?\t"))
         for x in range(3):
-            generate_cc_attributes(number_of_mean_commas, number_of_cov_commas, number_of_icov_commas)
+            generate_cc_attributes(
+                number_of_mean_commas, number_of_cov_commas, number_of_icov_commas
+            )
     else:
-        print('invalid attribute name >:(')
+        print("invalid attribute name >:(")
         break
 
-    repeat = input('do you have more to enumerate? y/n\t')
-print('thanks for using! check out my website: https://m3l.me ^-^')
+    repeat = input("do you have more to enumerate? y/n\t")
+print("thanks for using! check out my website: https://m3l.me ^-^")
--- converter.py
+++ converter.py
@@ -5,15 +5,15 @@
 import json
 from shutil import copyfile
 
-new_file_name = "7"
+new_file_name = "birthofcreation"
 header = ""
 just_nums = ""
 end_str = ""
 filtered_string = ""
-list_of_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-list_of_mums_and_separators = list_of_nums + ['.'] + ['-']
+list_of_nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+list_of_mums_and_separators = list_of_nums + ["."] + ["-"]
 
-with open(new_file_name + '.json') as json_file:
+with open(new_file_name + ".json") as json_file:
     json_data = json.load(json_file)
     json_values_as_list = json_data.values()
     values = str(json_values_as_list)
@@ -21,59 +21,118 @@ with open(new_file_name + '.json') as json_file:
         # this accounts for an issue I found when collecting the metadata information since digits can exist in
         # non-numerical variables like strings. this is not a very elegant way of going about it but it should work
         # since bpms should be between 1-3 digits.
-        if values[x] == 'u' and values[x + 1] == 'n' and values[x + 2] == 't':
-            if (values[x + 5] == ' ' and values[x + 6] in list_of_nums and values[x + 7] == ',') or \
-                    (values[x + 5] == ' ' and values[x + 6] in list_of_nums and values[x + 7] in list_of_nums and
-                     values[x + 8] == ',') or (values[x + 5] == ' ' and values[x + 6] in list_of_nums and
-                                               values[x + 7] in list_of_nums and values[x + 8] in list_of_nums and
-                                               values[x + 9] == ','):
-                just_nums += ', '
+        if values[x] == "u" and values[x + 1] == "n" and values[x + 2] == "t":
+            if (
+                (
+                    values[x + 5] == " "
+                    and values[x + 6] in list_of_nums
+                    and values[x + 7] == ","
+                )
+                or (
+                    values[x + 5] == " "
+                    and values[x + 6] in list_of_nums
+                    and values[x + 7] in list_of_nums
+                    and values[x + 8] == ","
+                )
+                or (
+                    values[x + 5] == " "
+                    and values[x + 6] in list_of_nums
+                    and values[x + 7] in list_of_nums
+                    and values[x + 8] in list_of_nums
+                    and values[x + 9] == ","
+                )
+            ):
+                just_nums += ", "
         # getting rid of var2, dmean2, loudness_ebu128, silence_rate_20dB, silence_rate_30dB, silence_rate_60dB,
         # and melbands128
-        elif (values[x - 1] == 'r' and values[x] == '2' and values[x + 1] == "'") or \
-                (values[x - 1] == 'n' and values[x] == '2' and values[x + 1] == "'") or \
-                (values[x - 1] == 'u' and values[x] == '1' and values[x + 1] == "2") or \
-                (values[x - 2] == 'u' and values[x - 1] == '1' and values[x] == '2' and values[x + 1] == "8") or \
-                (values[x - 3] == 'u' and values[x - 2] == '1' and values[x - 1] == '2' and values[x] == '8' and values[
-                    x + 1] == "'") or \
-                (values[x - 1] == '_' and values[x] == '2' and values[x + 1] == "0") or \
-                (values[x - 1] == '2' and values[x] == '0' and values[x + 1] == "d") or \
-                (values[x - 1] == '_' and values[x] == '3' and values[x + 1] == "0") or \
-                (values[x - 1] == '3' and values[x] == '0' and values[x + 1] == "d") or \
-                (values[x - 1] == '_' and values[x] == '6' and values[x + 1] == "0") or \
-                (values[x - 1] == '6' and values[x] == '0' and values[x + 1] == "d") or \
-                (values[x - 1] == 's' and values[x] == '1' and values[x + 1] == "2") or \
-                (values[x - 2] == 's' and values[x - 1] == '1' and values[x] == '2' and values[x + 1] == "8") or \
-                (values[x - 3] == 's' and values[x - 2] == '1' and values[x - 1] == '2' and values[x] == '8' and values[
-                    x + 1] == "'"):
-            just_nums += 'DEL, '
+        elif (
+            (values[x - 1] == "r" and values[x] == "2" and values[x + 1] == "'")
+            or (values[x - 1] == "n" and values[x] == "2" and values[x + 1] == "'")
+            or (values[x - 1] == "u" and values[x] == "1" and values[x + 1] == "2")
+            or (
+                values[x - 2] == "u"
+                and values[x - 1] == "1"
+                and values[x] == "2"
+                and values[x + 1] == "8"
+            )
+            or (
+                values[x - 3] == "u"
+                and values[x - 2] == "1"
+                and values[x - 1] == "2"
+                and values[x] == "8"
+                and values[x + 1] == "'"
+            )
+            or (values[x - 1] == "_" and values[x] == "2" and values[x + 1] == "0")
+            or (values[x - 1] == "2" and values[x] == "0" and values[x + 1] == "d")
+            or (values[x - 1] == "_" and values[x] == "3" and values[x + 1] == "0")
+            or (values[x - 1] == "3" and values[x] == "0" and values[x + 1] == "d")
+            or (values[x - 1] == "_" and values[x] == "6" and values[x + 1] == "0")
+            or (values[x - 1] == "6" and values[x] == "0" and values[x + 1] == "d")
+            or (values[x - 1] == "s" and values[x] == "1" and values[x + 1] == "2")
+            or (
+                values[x - 2] == "s"
+                and values[x - 1] == "1"
+                and values[x] == "2"
+                and values[x + 1] == "8"
+            )
+            or (
+                values[x - 3] == "s"
+                and values[x - 2] == "1"
+                and values[x - 1] == "2"
+                and values[x] == "8"
+                and values[x + 1] == "'"
+            )
+        ):
+            just_nums += "DEL, "
 
-        elif values[x] in list_of_nums and (values[x + 1] == ',' or values[x + 1] == '}' or values[x + 1] == ']'):
-            just_nums += values[x] + ', '
+        elif values[x] in list_of_nums and (
+            values[x + 1] == "," or values[x + 1] == "}" or values[x + 1] == "]"
+        ):
+            just_nums += values[x] + ", "
         # accounting for e+ and e- values
-        elif (values[x] == 'e' and values[x + 1] == '+') or (values[x] == 'e' and values[x + 1] == '-'):
+        elif (values[x] == "e" and values[x + 1] == "+") or (
+            values[x] == "e" and values[x + 1] == "-"
+        ):
             just_nums += values[x] + values[x + 1]
         elif values[x] in list_of_mums_and_separators:
-            if values[x - 1] != 'e' and values[x + 1] != '-':
+            if values[x - 1] != "e" and values[x + 1] != "-":
                 just_nums += values[x]
         # appending the end with the key signature
-        elif values[x] == 'd' and values[x + 1] == 's' and values[x + 2] == '_' and values[x + 3] == 'k' and \
-                values[x + 4] == 'e':
+        elif (
+            values[x] == "d"
+            and values[x + 1] == "s"
+            and values[x + 2] == "_"
+            and values[x + 3] == "k"
+            and values[x + 4] == "e"
+        ):
             # this accounts for keys that are flat (b) or sharp (#)
-            if values[x + 5] == 'b' or values[x + 5] == '#':
+            if values[x + 5] == "b" or values[x + 5] == "#":
                 just_nums += ", '" + values[x + 10] + values[x + 11] + "', "
             else:
                 just_nums += ", '" + values[x + 10] + "', "
         # appending the end with whether the key signature is a major/minor key
         # (lucky that both 'major' and 'minor' hve 5 characters)
-        elif values[x] == 'd' and values[x + 1] == 's' and values[x + 2] == '_' and values[x + 3] == 's' and values[
-            x + 4] == 'c':
-            just_nums += "'" + values[x + 12] + values[x + 13] + values[x + 14] + values[x + 15] + values[x + 16] + "'"
+        elif (
+            values[x] == "d"
+            and values[x + 1] == "s"
+            and values[x + 2] == "_"
+            and values[x + 3] == "s"
+            and values[x + 4] == "c"
+        ):
+            just_nums += (
+                "'"
+                + values[x + 12]
+                + values[x + 13]
+                + values[x + 14]
+                + values[x + 15]
+                + values[x + 16]
+                + "'"
+            )
 
-    just_nums_as_list = just_nums.split(', ')
+    just_nums_as_list = just_nums.split(", ")
     # deleting trash values
-    while 'DEL' in just_nums_as_list:
-        index = just_nums_as_list.index('DEL')
+    while "DEL" in just_nums_as_list:
+        index = just_nums_as_list.index("DEL")
         del just_nums_as_list[index]
     # removes weird blank element
     del just_nums_as_list[-3]
@@ -86,22 +145,22 @@ with open(new_file_name + '.json') as json_file:
         # the actual number generated by Essentia should be less than 25 digits (including '.', 'e+', and 'e-')
         # the compounded conditional just removes the trailing comma
         if len(just_nums_as_list[x]) <= 25 and x != len(just_nums_as_list) - 1:
-            filtered_string += str(just_nums_as_list[x]) + ', '
+            filtered_string += str(just_nums_as_list[x]) + ", "
         elif len(just_nums_as_list[x]) <= 25:
             filtered_string += str(just_nums_as_list[x])
 
-    print('raw json data:                ', json_data)
-    print('json data as list:            ', json_values_as_list)
-    print('json data as a string:        ', values)
-    print('just the numbers as a string: ', just_nums)
-    print('just the numbers as list:     ',  just_nums_as_list)
-    print('filtered numbers as string:   ', filtered_string)
+    print("raw json data:                ", json_data)
+    print("json data as list:            ", json_values_as_list)
+    print("json data as a string:        ", values)
+    print("just the numbers as a string: ", just_nums)
+    print("just the numbers as list:     ", just_nums_as_list)
+    print("filtered numbers as string:   ", filtered_string)
 
     # outputting to new file for weka
     new_file_name += ".arff"
-    copyfile('base_first.arff', new_file_name)
+    copyfile("base_first.arff", new_file_name)
     f = open(new_file_name, "a+")
-    j = open('base_second.arff', "r")
+    j = open("base_second.arff", "r")
     data = j.read()
     j.close()
     f.write(data)
